cc8216801238616bce3ac6a684f7628b9f7bbe27
Sam Popowich <spopowich@gmail.com>
2025-03-20 06:36:39 -0500

    Add failing test for assign ticket.

--- system_test.py
+++ system_test.py
@@ -15,3 +15,10 @@ def test_create_ticket():
   assert len(ts.list_tickets()) == 0
   ts.create_ticket("Attila the Hun", "Horse stopped running.")
   assert len(ts.list_tickets()) == 1
+
+def test_assign_ticket():
+  ts.create_agent("Sam Popowich", "Library Systems")
+  ts.create_ticket("Attila the Hun", "Horse stopped running.")
+  assert len(ts.agents[0].tickets) == 0
+  ts.assign_ticket(ts.agents[0], ts.tickets[0])
+  assert len(ts.agents[0].tickets) == 1
